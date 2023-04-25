621eaf55c778521145395ed144b9acb798f1588a
Ankur Pandey <ankur.pandey@xrda3.com>
2023-04-25 19:52:26 +0530

    Relational oprator
    To understand the use of relational operator
    
    Signed-off-by: Ankur Pandey <ankur.pandey@xrda3.com>

---
+++ relational_operator.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+   int a,b;
+   printf("Please Enter numbers \n");
+   scanf("%d%d",&a,&b);
+   if(a>b)
+   {
+       printf("%d is greater than %d\n",a ,b);
+   }
+   if(a<=b)
+   {
+       printf("%d is less than or equal to %d\n",a,b);
+   }
+   if(a>=b)
+   {
+       printf("%d is greater than or equal to %d\n",a,b);
+   }
+   if(a<b)
+   {
+       printf("%d is less than %d\n",a,b);
+   }
+   if(a!=b)
+   {
+       printf("%d is not equal to %d\n",a,b);
+   }
+   if(a==b)
+   {
+       printf("%d is equal to %d\n",a,b);
+   }
+    return 0;
+}
+
