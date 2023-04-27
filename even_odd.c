3ed05047700fe09dc9d3a30de5d3e0c1aac63c61
Ankur Pandey <ankur.pandey@xrda3.com>
2023-04-27 20:40:06 +0530

    Even/Odd
    change in printf message
    
    Signed-off-by: Ankur Pandey <ankur.pandey@xrda3.com>

--- even_odd.c
+++ even_odd.c
@@ -1,9 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    
    int number1;
-   printf ("Please enter two numbers two compare\n");
+   printf ("Please enter a number\n");
    scanf("%d",&number1);
    if(number1%2==0)
       printf("%d is Even\n",number1);
