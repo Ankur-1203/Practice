16426576aa7acc576c0919a3b81e1c5c3e29b9f8
Ankur Pandey <ankur.pandey@xrda3.com>
2023-04-27 20:09:51 +0530

    Compare two numbers
    take input from user and compare numbers are equal or not
    
    Signed-off-by: Ankur Pandey <ankur.pandey@xrda3.com>

---
+++ compare_two_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+ int number1,number2;
+ printf("Enter Two numbers two compare\n");
+ scanf("%d%d",&number1,&number2);
+ if(number1==number2)
+	 printf("Both are equal");
+ else
+	 printf("Not Equal");
+return 0;
+}
